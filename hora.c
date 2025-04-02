b7ee49084f049a351a59f300aad7f651edcd5951
EmilyFerreira10 <emilyferreira@alunos.utfpr.edu.br>
2025-04-02 09:09:58 -0300

    Update hora.c

--- hora.c
+++ hora.c
@@ -3,19 +3,19 @@ int main()
 {
 
    int hora, min, seg;
-   float aux = 5.5;
 
    printf("Digite a hora, minuto e segundo:") ;
    scanf("%d %d %d", &hora, &min, &seg);
 
    //se hora maior que 1 e minuto maior que 15
-   printf("quantidade de segundos: 59 ");
+   if (hora > 1 && min> 15){
+   printf("quantidade de segundos: %d\n", (hora * 3600)+(min*60)+seg);
+   } else {
    //caso contrario
-   printf("quantidade de segundos: 22");
+   printf("quantidade de segundos: ");
 
 
    printf("horario: %02d:%02d:%02d\n", hora, min, seg);
-   printf("float com duas casas: %.2f", aux);
-
+}
    return 0;
 }
